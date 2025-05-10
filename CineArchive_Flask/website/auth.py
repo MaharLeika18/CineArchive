c72cb3876f8fd2f18de923fd32274023de5adeb5
MaharLeika18 <168389818+MaharLeika18@users.noreply.github.com>
2025-05-10 17:39:21 +0800

    Integrated Kinah's pages
    Fixed some of the code so now the account related pages are functional. Also fixed some of the template file directory.

--- CineArchive_Flask/website/auth.py
+++ CineArchive_Flask/website/auth.py
@@ -33,7 +33,7 @@ def login():
             db.session.rollback()
             flash(f"Login error: {e}", 'danger')
 
-    return render_template('login.html')
+    return render_template('login.html', back_url=request.referrer)
 
 @auth.route('/logout')
 @login_required
